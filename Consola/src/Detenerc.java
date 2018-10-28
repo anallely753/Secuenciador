2e256110b33994889c69b51980527de1919bd916
anallely753 <anallely.0710@gmail.com>
2018-10-28 16:41:32 -0600

    Proyecto terminado

---
+++ Consola/src/Detenerc.java
@@ -0,0 +1,7 @@
+
+import java.io.Serializable;
+
+
+public class Detenerc implements Serializable{
+    public Boolean parar = false;
+}
---
+++ Consola/src/Secuenciador.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class Secuenciador implements Serializable{
+    public ArrayList<Boolean> lin1 = new ArrayList<Boolean>();
+    public ArrayList<Boolean> lin2 = new ArrayList<Boolean>();
+    public ArrayList<Boolean> lin3 = new ArrayList<Boolean>();
+    public ArrayList<Boolean> lin4 = new ArrayList<Boolean>();
+    public int bpm = 0;
+    public Boolean loop = false;
+    public int repeat;
+   
+    public Secuenciador(){
+        llenarFalse(lin1);
+        llenarFalse(lin2);
+        llenarFalse(lin3);
+        llenarFalse(lin4);
+    }
+    public void llenarFalse(ArrayList<Boolean> lin){
+       for(int i=0 ; i<8 ; i++){
+            lin.add(false);
+        }
+    }
+    
+}
